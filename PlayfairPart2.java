06708999fca386d5b2db646dd2643fc2c94ec819
Modhi-hash <moodybandarf16@gmail.com>
2026-04-30 17:09:56 +0300

    PlayfairPart2.java

--- PlayfairPart2.java
+++ PlayfairPart2.java
@@ -1,7 +1,4 @@
- /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.playfairmaingui;
 
 /**
@@ -11,7 +8,7 @@ package com.mycompany.playfairmaingui;
 public class PlayfairPart2 {
     
 
-    // Find row and column of a character in the matrix
+  
     public static int[] findPosition(char[][] matrix, char c) {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
@@ -20,7 +17,7 @@ public class PlayfairPart2 {
         } 
         return null;
     }
-    // Encryption: Shift characters +1
+   
     public static String encrypt(java.util.List<String> pairs, char[][] matrix) {
         StringBuilder result = new StringBuilder();
         for (String pair : pairs) {
@@ -28,7 +25,7 @@ public class PlayfairPart2 {
         }
         return result.toString();
     }
-     // Decryption: Shift characters +4
+    
     public static String decrypt(String text, char[][] matrix) {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < text.length(); i += 2) {
@@ -42,15 +39,15 @@ public class PlayfairPart2 {
         int[] p1 = findPosition(matrix, a);
         int[] p2 = findPosition(matrix, b);
 
-        if (p1[0] == p2[0]) // Rule 1: Same row
+        if (p1[0] == p2[0]) 
         { 
             return "" + matrix[p1[0]][(p1[1] + shift) % 5] + matrix[p2[0]][(p2[1] + shift) % 5];
             
-        } else if (p1[1] == p2[1]) // Rule 2: Same column
+        } else if (p1[1] == p2[1]) 
         { 
             return "" + matrix[(p1[0] + shift) % 5][p1[1]] + matrix[(p2[0] + shift) % 5][p2[1]];
             
-        } else // Rule 3: Rectangle swap
+        } else 
         { 
             return "" + matrix[p1[0]][p2[1]] + matrix[p2[0]][p1[1]];
         }
